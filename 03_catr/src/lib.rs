86293bf429b6f8f3dce0da6bf344b1cf4a3d7006
Tian Yu <gasnus@gmail.com>
2022-10-02 14:54:40 +0200

    Chapter 3 On the Catwalk

---
+++ 03_catr/src/lib.rs
@@ -0,0 +1,90 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::{Arg, ArgAction, Command};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .action(ArgAction::Append)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches
+            .get_many::<String>("files")
+            .unwrap()
+            .map(|f| f.to_owned())
+            .collect(),
+        number_lines: matches.get_flag("number"),
+        number_nonblank_lines: matches.get_flag("number_nonblank"),
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
